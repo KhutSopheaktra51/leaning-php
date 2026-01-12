355ef185006f76b209a6fdf658a7836f315c2f76
AlEAN7447 <khuttra422@gmail.com>
2026-01-12 10:28:13 +0700

    edit include

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include './include/header.inc.php';
+include './include/navbar.inc.php';
+// include './page/' . $_GET['page'] . '.php';
+
+$available_pages = ['login', 'register'];
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    echo $_GET['page'];
+    if (in_array($page, $available_pages)) {
+        include './page/' . $page . '.php';
+    } else {
+        echo '<h1>Page ng ot mean tee</h1>';
+    }
+} else {
+    echo '<h1> Home Page,</h1>';
+}
+
+include './include/footer.inc.php';
+?>
---
+++ page/register.php
@@ -0,0 +1,22 @@
+
+    <form method="post" action="./?page=register" class="col-md-6 col-lg-6 col-sm-6 col-xl-6 col-xs-6 mx-auto my-5">
+        <h3>Register Page</h3>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input name="username" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Comfirm Password</label>
+    <input name="confirm_password" type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
